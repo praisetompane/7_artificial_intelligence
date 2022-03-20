806f1afe57a0af99a135281c946dfa63ef878cd8
Praise Tompane <praisetompane@gmail.com>
2022-03-20 22:13:28 +0200

    dice roll output simulation

--- 0_datascience/4_stochastic_thinking/3_roll_dice_simulation.py
+++ 0_datascience/4_stochastic_thinking/3_roll_dice_simulation.py
@@ -6,13 +6,15 @@ Example implementation of stochastic process.
 '''
 import random
 
-possible_numbers=[1,2,3,4,5,6]
+possible_numbers = [1, 2, 3, 4, 5, 6]
 
 def roll_dice_underdetermined():
     return possible_numbers[1]
 
+
 def roll_dice_stochastic():
     return random.choice(possible_numbers)
+    # random.choice() has a uniform distribution = equally probable to choose any number
 
 def test_roll_dice_stochastic(rolls):
     result = ''
@@ -26,7 +28,6 @@ def test_roll_dice_underdetermined(rolls):
         result += str(roll_dice_underdetermined())
     print(result)
 
-#random.choice() has a uniform distribution = equally probable to choose any number
 
 '''
 Given:
@@ -38,5 +39,36 @@ Given:
             divide by
             all possible events: all possible results of 5 rolls= 6^5
                 e.g. 11111, 11112,11115 ... 21342,12342,
-        answer = 1/6^5 = unlikely
+        answer = 1/6^rolls = unlikely
 '''
+
+
+def rum_sim(trials, target_text):
+    total_occurence = 0
+    result = ''
+    for t in range(trials):
+        print(f'beginning trial {t}')
+        for roll in range(len(target_text)):
+            result += str(roll_dice_stochastic())
+        if(result == target_text):
+            total_occurence += 1
+        print(f'result = {result}')
+        result = ''
+        print(f'done with trials {t}')
+    return total_occurence
+
+random.seed(0)
+def simulate():
+    trials = 1000000
+    possible_values = 6
+    target_text = '11111'
+    precision = 8
+    total_occurence = rum_sim(trials, target_text)
+    print('beginning simulation')
+    print(
+        f'sample(estimated) probability of {target_text} is {round(total_occurence/trials, precision)}')
+    print(
+        f'actual probability of {target_text} is {round(1/possible_values**len(target_text), precision)}')
+
+
+simulate()
