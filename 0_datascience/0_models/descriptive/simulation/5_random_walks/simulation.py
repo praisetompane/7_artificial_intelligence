1a865bc90423b3c57f1eef41b94b534aeced1fc3
Praise Tompane <praisetompane@gmail.com>
2022-03-28 04:25:15 +0200

    basic plotting and ML book reference

--- 0_datascience/0_models/descriptive/simulation/5_random_walks/simulation.py
+++ 0_datascience/0_models/descriptive/simulation/5_random_walks/simulation.py
@@ -2,6 +2,7 @@ import random
 from field import Field
 from location import Location
 from drunk import UsualDrunk
+import matplotlib.pyplot as plt
 
 '''
     SWITCH ON/UNCOMMENT TO GET DETERMINISTIC BEHAVIOUR. FOR TESING
@@ -11,6 +12,8 @@ random.seed(0)
     references:
         Guttag, John. Introduction to Computation and Programming Using Python: With Application to Understanding Data. 2nd ed. MIT Press, 2016. ISBN: 9780262529624.page 221
 '''
+
+
 def walk(field, drunk, steps):
     initial_location = field.retrieve_location(drunk)
     for _ in range(steps):
@@ -38,9 +41,13 @@ def drunk_test(drunk_class, trials, walk_lengths):
         print(f'Max: {round(max(distances),2)}')
         print(f'Min: {round(min(distances),2)}')
         print('\n')
+        print(f'# distances {trials}')
+
+        plt.scatter(distances, range(trials))
+        plt.show()
 
 
 print('Sanity Checks')
-drunk_test(UsualDrunk, 100, (0, 1))
+#drunk_test(UsualDrunk, 100, (0, 1))
 print('Done Sanity Checks\n')
 drunk_test(UsualDrunk, 100, (10, 100, 1000, 10000))
--- README.md
+++ README.md
@@ -11,6 +11,8 @@
 - MIT 6.0002 Introduction to Computational Thinking and Data Science
 - https://www.youtube.com/channel/UCX6b17PVsYBQ0ip5gyeme-Q
 
-# community
+- Hands-On Machine Learning with Scikit-Learn, Keras and TensorFlow by Aurélien Géron (O’Reilly). Copyright 2019 Aurélien Géron, 978-1-492-03264-9.
 
+# community
 - open AI
+- https://www.kaggle.com
